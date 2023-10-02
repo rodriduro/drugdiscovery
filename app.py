bec4e86e8525a81a563a33fdae781810313e432c
rodrigoduro <rodriduro@icloud.com>
2023-10-02 19:53:46 +0100

    fix for libxrender1 on streamlit cloud

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 Docker Container: https://hub.docker.com/r/continuumio/anaconda3
 RDKit Installation: https://www.rdkit.org/docs/Install.html
 """
+import subprocess
+p = subprocess.run("sudo apt-get install libxrender1")
+
 import mols2grid
 import pandas as pd
 import streamlit as st
